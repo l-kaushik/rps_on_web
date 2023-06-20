ca8fcb8ed099a8566cfdc215e7edb2b7cca5fda8
l-kaushik <contact.lokeshkaushik@gmail.com>
2023-06-20 16:48:33 +0530

    Fixed bugs with game and getPlayerChoice
    game function was not taking input for 5 rounds, fixed this by calling both choices function in it.
    getPlayerChoice was going in infinite loop when encounter wrong inputs, fixed this by getting input inside the loop.
    
    Also, added player and computer score and checks the score at the end of the game and on comparison declare the wins.

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
-function getComputerChoice()
-{
+function getComputerChoice() {
     let selectionIndex = Math.floor(Math.random() * 3);
 
-    if(selectionIndex == 0){
+    if (selectionIndex == 0) {
         return "Rock";
     }
-    else if(selectionIndex == 1){
+    else if (selectionIndex == 1) {
         return "Paper";
     }
-    else{
+    else {
         return "Scissors";
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    
-    if (playerSelection == computerSelection){
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
         return `Both choose ${playerSelection}, so its a tie`;
     }
-    else if ((playerSelection == "Rock" && computerSelection == "Scissors")|| (playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Paper" && computerSelection == "Rock")){
+    else if ((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Paper" && computerSelection == "Rock")) {
         playerScore += 1;
         return `${playerSelection} beats ${computerSelection} and player got a point`;
     }
-    else{
+    else {
         computerScore += 1;
         return `${computerSelection} beats ${playerSelection} and computer got a point`;
     }
 }
 
-function game(){
-    for (let i = 1; i<=5; i++){
+function game() {
+    for (let i = 1; i <= 5; i++) {
         console.log(`Round ${i}: `);
-        playRound(playerSelection, computerSelection);
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
     }
 }
 
-function getPlayerChoice(){
+function getPlayerChoice() {
     let playerSelection = prompt("Enter your selection: ").toLowerCase();
-playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
 
+    while (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
+
+        console.log(`Invalid input "${playerSelection}", must be "rock" or "paper" or "scissors"`);
+
+        playerSelection = prompt("Enter your selection: ");
+        playerSelection = playerSelection.toLowerCase();
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+   
+    }
     return playerSelection;
 }
 
 var playerScore = 0, computerScore = 0;
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
+// let computerSelection = getComputerChoice();
+// let playerSelection = getPlayerChoice();
 
+game()
 
-if(playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors"){
-    console.log(`Invalid input "${playerSelection}", must be "rock" or "paper" or "scissors"`);
-}else{
-
-    game()
-
-    if (computerScore == playerScore){
-        console.log(`both got same points ${computerScore}`);
-    }
-    else if(playerScore > computerScore){
-        console.log(`Player wins by ${computerScore - playerScore}`);
-    }
-    else
-    {
-        console.log(`Computer wins by ${playerScore - computerScore}`);
-    }
-    
+if (computerScore == playerScore) {
+    console.log(`both got same points ${computerScore}`);
+}
+else if (playerScore > computerScore) {
+    console.log(`Player wins by ${playerScore - computerScore}`);
+}
+else {
+    console.log(`Computer wins by ${computerScore - playerScore}`);
 }
 
 
