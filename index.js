daa2d46a5c065d1da82f3a70cd7dd6eb5260a781
l-kaushik <contact.lokeshkaushik@gmail.com>
2023-06-20 16:24:27 +0530

    Created playRound function
    This function will be use to play a single round and return the result of the round in string.

--- index.js
+++ index.js
@@ -6,11 +6,38 @@ function getComputerChoice()
         return "Rock";
     }
     else if(selectionIndex == 1){
-        return "Paper"
+        return "Paper";
     }
     else{
-        return "Scissor"
+        return "Scissors";
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    
+    if (playerSelection == computerSelection){
+        return `Both choose ${playerSelection}, so its a tie`;
+    }
+    else if ((playerSelection == "Rock" && computerSelection == "Scissors")|| (playerSelection == "Scissors" && computerSelection == "Paper") || (playerSelection == "Paper" && computerSelection == "Rock")){
+        return `${playerSelection} beats ${computerSelection} and player got a point`;
+    }
+    else{
+        return `${computerSelection} beats ${playerSelection} and computer got a point`;
+    }
+}
+
+
+let computerSelection = getComputerChoice();
+
+let playerSelection = prompt("Enter your selection: ").toLowerCase();
+playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
+if(playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors"){
+    console.log(`Invalid input "${playerSelection}", must be "rock" or "paper" or "scissors"`);
+}else{
+
+    console.log(playRound(playerSelection,computerSelection));
+    
+}
+
+
